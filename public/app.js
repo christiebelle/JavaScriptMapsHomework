31effb7cfb7d300ddca1a5b352ee633d505b5d00
Christiebelle <cslester@outlook.com>
2018-04-10 16:27:50 +0100

    renamed the mapdiv in css file

---
+++ public/app.js
@@ -0,0 +1,15 @@
+const initialise = function(){
+
+    const container = document.querySelector('#mapDiv');
+    const center = {lat: 39.901284, lng: -75.171990};
+    const zoom = 14;
+
+    const map = new MapMaker(container, center, zoom);
+    map.addMarker(center);
+    map.addClickEvent();
+
+    const changeLocation = document.getElementById('change-location');
+    changeLocation.addEventListener('click', map.changeLocation);
+};
+
+window.addEventListener('DOMContentLoaded', initialise);
